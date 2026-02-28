58e603c900f8169265bcaa725eb65bd98f38fa60
Alessio Flore <florealessio54@gmail.com>
2026-02-28 20:29:29 +0100

    Fix modulo operation for value in binary conversion

--- 1680-ConsecutiveBinNumbers.py
+++ 1680-ConsecutiveBinNumbers.py
@@ -5,6 +5,5 @@ class Solution:
         for i in range(n+1):
             value += str(bin(i)[2:])
         value = int(value, 2)
-        if(value > pow(10, 9) + 7):
-            value = value % (pow(10,9) + 7)
+        value = value % (pow(10,9) + 7)
         return value 
